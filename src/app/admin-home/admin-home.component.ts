e7cc9898740037177c14556d9ba90b961d750a37
soutionscience <marshnjagi@yahoo.com>
2019-01-28 13:54:10 +0300

    added create campaign

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Web3ServiceService } from '../services/web3-service.service';
+
 
 @Component({
   selector: 'app-admin-home',
@@ -6,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit {
+  user: String;
+  showWarning: Boolean;
+  trpcURL: String;
 
-  constructor() { }
+  constructor(private webService: Web3ServiceService) {
+    this.trpcURL ="http://localhost:8545";
+    
+  
+   }
 
   ngOnInit() {
+    this.getAccountsInfo()
+  
+     
   }
+  getAccountsInfo(){
+    this.webService.getAccount().subscribe((resp)=>{
+     if(resp.length <1){
+       this.showWarning = true;
+       console.log('resp is ', resp);
+       this.user = resp[0];
+       
+     }else{
+       this.user= resp[0]
+       this.showWarning = false
+       console.log(this.user)
+       
+     }
+    })
+  }
+  newCampaign(){
+    this.showWarning = !this.showWarning
+// this.getAccountsInfo();
+this.webService.createNewLeague("0xde4973d1d1c741916f283cde758cbb4eba6c032e", "4", "1000000")
+.subscribe(resp=>{
+  console.log('responce ', resp )
+})
+  
+
+  }
+  connectTRPC(){
+    console.log("connecting to test rpc ", this.trpcURL)
+    this.webService.connecTRPC(this.trpcURL)
+    this.getAccountsInfo()
+  }
+  cancel(){
+    console.log('canceling')
+    this.showWarning= false
+  }
+  instance(){
+    console.log('creating instance')
+    let instance = this.webService.createContractInstance("0xde4973d1d1c741916f283cde758cbb4eba6c032e", "test");
+    // console.log('what is in instance ', instance)
+  }
+  getLeagues(){
+    console.log('getting leagues');
+    this.webService.getAllLeagues('0xde4973d1d1c741916f283cde758cbb4eba6c032e', '1000000')
+    .subscribe(resp=>{
+      console.log('leagues returned ', resp)
+    })
+  }
+
 
 }
---
+++ src/app/services/web3-service.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Observable } from '../../../node_modules/rxjs';
+
+
+let leagueFactoryContract = require('../../ethereum/contracts/build/LeagueFactory.json');
+let LeagueContract = require('../../ethereum/contracts/build/League.json')
+
+
+
+declare var window: any; // declare window
+declare var require: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Web3ServiceService {
+ public web3: Web3
+  constructor() { this.checkWeb3Instance()}
+
+  //check if web3 has been injected to browser
+  checkWeb3Instance(){
+    if (typeof window.web3 !== 'undefined') {
+      console.warn(
+        'Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask'
+      );
+      // Use Mist/MetaMask's provider
+      this.web3 = new Web3(window.web3.currentProvider);
+      return true;
+    } else{
+      console.warn(
+        'No web3 detected. Falling back to ${environment.HttpProvider}. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+      );
+      return false;
+      //  fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      // this.web3 = new Web3(
+      //   new Web3.providers.HttpProvider('http://localhost:8545')
+      // );
+    }
+   }
+   getUserInfo(){
+
+   }
+   connecTRPC(url){
+    console.log("connecting to .. ", url)
+    this.web3 = new Web3(new Web3.providers.HttpProvider(url))
+
+   }
+
+   getAccount(): Observable<any>{
+    return Observable.create(observer=>{
+      this.web3.eth.getAccounts((err, accounts)=>{
+        if(err){
+          console.log('err getting accounts')
+          observer.error('there was an error getting your accounts');
+          
+        }else{
+          observer.next(accounts);
+          observer.complete;
+        }
+      })
+    })
+  }
+    // create a contract instance helper function.
+    //used to createNewLeague and more...
+    createContractInstance(addr, contractJson){
+      let instance;
+      let abiDef = contractJson.abi
+      let contract = this.web3.eth.contract(abiDef);
+      instance = contract.at(addr);
+      console.log('instance created' ,instance)
+      return instance
+  
+    }
+    createNewLeague(addr, amount, gasToUse):Observable<any>{
+    return Observable.create(observer=>{
+      let instance = this.createContractInstance(addr, leagueFactoryContract);
+      let transactionObject={
+        from: this.web3.eth.coinbase,
+        gas:gasToUse
+      }
+   
+
+      instance.deployLeague.sendTransaction(amount, transactionObject, (err, result)=>{
+        if(err){
+          console.log('error deploying league');
+          observer.error(err)
+        }else{
+          console.log('deployed league successfully')
+          observer.next(result)
+          observer.complete()
+        }
+      })
+
+    })
+    }
+    getAllLeagues(addr, gasToUse):Observable<any>{
+    return Observable.create(observer=>{
+      let instance = this.createContractInstance(addr, leagueFactoryContract);
+      let transactionObject ={
+        from: this.web3.eth.coinbase,
+        gas: gasToUse
+      }
+      //use instance to get all leagues
+      console.log('creating instance ', instance)
+    
+
+      instance.GetAllLeagues.sendTransaction(transactionObject, (err, result)=>{
+        if(err){
+          console.log('error getting leagues')
+          observer.error(err)
+        }else{
+          console.log('got all leagues')
+          observer.next(result);
+          observer.complete()
+        }
+      })
+
+    })
+    }
+
+}
